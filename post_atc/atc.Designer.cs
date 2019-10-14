a6ec06c94cb155252a3eae649d36cc3a269a405d
zhongning <xyzning@qq.com>
2019-10-14 19:03:17 +0800

    get test result

--- post_atc/atc.Designer.cs
+++ post_atc/atc.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(atc));
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -38,34 +37,21 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Green;
-            this.button1.Location = new System.Drawing.Point(59, 215);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(133, 194);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 51);
+            this.button1.Size = new System.Drawing.Size(175, 69);
             this.button1.TabIndex = 0;
-            this.button1.Text = "PASS";
+            this.button1.Text = "P O S T";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.Red;
-            this.button2.Location = new System.Drawing.Point(260, 215);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 51);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "FAIL";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(164, 109);
-            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(202, 28);
+            this.textBox1.Size = new System.Drawing.Size(202, 26);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -86,7 +72,6 @@
             this.ClientSize = new System.Drawing.Size(419, 304);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -106,7 +91,6 @@
         public System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         public System.Windows.Forms.Button button1;
-        public System.Windows.Forms.Button button2;
     }
 }
 
--- post_atc/atc.cs
+++ post_atc/atc.cs
@@ -31,23 +31,17 @@ namespace post_atc
         MySqlConnection cnn;
         public void atc_Load(object sender, EventArgs e)
         {
-            string[] config = File.ReadAllLines(@"C:\config\config.ini");
-            string[] server = config[9].Split('=');
-            string ip = server[1];
-            string[] database = config[10].Split('=');
-            string dbname = database[1];
-            string[] user = config[11].Split('=');
-            string usname = user[1];
-            string[] pw = config[12].Split('=');
-            string pswd = pw[1];
-            string[] port = config[13].Split('=');
-            string pot = port[1];
-            string[] tablename = config[14].Split('=');
-            read_config.tbname = tablename[1];
+            //string[] config = File.ReadAllLines(@"C:\config\config.ini");
+            string ip = "10.18.255.116";
+            string dbname = "deming";
+            string usname = "deming";
+            string pswd = "A8ZtK*7febEX";
+            string pot = "3306";
+            read_config.tbname = "rosetta_US130_TMO_2000_0917";
             connectString = "server=" + ip + ";database=" + dbname + ";uid=" + usname + ";pwd=" + pswd + ";port=" + pot;
             cnn = new MySqlConnection(connectString);
         }
-
+    
        
         public void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -56,38 +50,88 @@ namespace post_atc
 
         public void button1_Click(object sender, EventArgs e)
         {
-            if(textBox1.Text.Length == 0)
+            if (textBox1.Text.Length == 0)
             {
                 MessageBox.Show("请输入正确的SN");
             }
             else
             {
+                string _file = "C:\\orion\\rosetta-atc\\logs\\complete";
+                string sn = textBox1.Text;
                 try
                 {
-                    cnn.Open();
-                    string sn = textBox1.Text;
-                    string select_sn = "select * from " + read_config.tbname + " where SN=" + "'" + sn + "'" + " and FullTestResult=" + read_config.result1;
-                    MySqlCommand cmd1 = new MySqlCommand(select_sn, cnn);
-                    if (cmd1.ExecuteScalar() == null)
+                    string[] files = Directory.GetFiles(_file, (sn + "*_APxResult.csv"));
+                    List<string> ssss = files.OrderBy(ss => new FileInfo(ss).CreationTime).ToList();
+                    string report = ssss.Last();
+                    //MessageBox.Show(report);
+                    string[] test = File.ReadAllLines(report);
+                    string[] haha = test[3].Split(',');
+                    if (haha[4] == "Failed")
                     {
-                        MessageBox.Show("整机功能测试未完成");
+                        //MessageBox.Show(haha[4]);
+                        try
+                        {
+                            cnn.Open();
+                            //string sn = textBox1.Text;
+                            string select_sn = "select * from " + read_config.tbname + " where SN = " + "'" + sn + "'" + " and FullTestResult =" + read_config.result1;
+                            MySqlCommand cmd1 = new MySqlCommand(select_sn, cnn);
+                            if (cmd1.ExecuteScalar() == null)
+                            {
+                                MessageBox.Show("整机功能测试未完成");
+                            }
+                            else
+                            {
+                                string post = "update " + read_config.tbname + " set TestCaseATC = 'Fail' where SN=" + "'" + sn + "'";
+                                MySqlCommand cmd = new MySqlCommand(post, cnn);
+                                cmd.ExecuteNonQuery();
+                                MessageBox.Show("上传成功!测试结果为Fail.");
+                                textBox1.Text = "";
+                            }
+                        }
+                        catch (MySqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        finally
+                        {
+                            cnn.Close();
+                        }
                     }
-                    else
+                    else if (haha[4] == "Passed")
                     {
-                        string post = "update "+ read_config.tbname + " set TestCaseATC = 'Pass' where SN=" + "'" + sn + "'";
-                        MySqlCommand cmd = new MySqlCommand(post, cnn);
-                        cmd.ExecuteNonQuery();
-                        MessageBox.Show("上传成功!测试结果为Pass.");
-                        textBox1.Text = "";
+                        //MessageBox.Show(haha[4]);
+                        try
+                        {
+                            cnn.Open();
+                            //string sn = textBox1.Text;
+                            string select_sn = "select * from " + read_config.tbname + " where SN=" + "'" + sn + "'" + " and FullTestResult=" + read_config.result1;
+                            MySqlCommand cmd1 = new MySqlCommand(select_sn, cnn);
+                            if (cmd1.ExecuteScalar() == null)
+                            {
+                                MessageBox.Show("整机功能测试未完成");
+                            }
+                            else
+                            {
+                                string post = "update " + read_config.tbname + " set TestCaseATC = 'Pass' where SN=" + "'" + sn + "'";
+                                MySqlCommand cmd = new MySqlCommand(post, cnn);
+                                cmd.ExecuteNonQuery();
+                                MessageBox.Show("上传成功!测试结果为Pass.");
+                                textBox1.Text = "";
+                            }
+                        }
+                        catch (MySqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        finally
+                        {
+                            cnn.Close();
+                        }
                     }
                 }
-                catch (MySqlException ex)
+                catch
                 {
-                    MessageBox.Show(ex.Message);
-                }
-                finally
-                {
-                    cnn.Close();
+                    MessageBox.Show("请检查complete.");
                 }
             }
         }
